9718440098e53778966119f1eb53b5f6953760c9
Marcdnd <marcdnd@gmail.com>
2015-04-07 00:44:35 -0300

    Fixando alguns erros

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ data_files += [
 setup(
     name="Electrum-CESC",
     version=version.ELECTRUM_VERSION,
-    install_requires=['slowaes', 'ecdsa>=0.9', 'cesc_scrypt'],
+    install_requires=['slowaes', 'ecdsa>=0.9', 'ltc_scrypt'],
     package_dir={
         'electrum_cesc': 'lib',
         'electrum_cesc_gui': 'gui',
